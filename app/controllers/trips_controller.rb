83f2e444367bed116396af85e7b51b14fe567291
elijah johnston <eli@millinno.com>
2017-11-20 17:20:43 -0200

    changed routes for trips, and corrected the params for trips controller to whitelist instead of blacklist/

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,11 +1,11 @@
 class TripsController < ApplicationController
-  before_action :set_bunker, only: [:edit, :show, :update, :destroy]
+  before_action :set_trip, only: [:edit, :show, :update, :destroy]
 
   def create
     @trip = Trip.new(trip_params)
-    @bunker = Bunker.find(params[:bunker_id])
-    @user = current_user.id
-    @booking_date = Date.today
+    @trip.bunker = Bunker.find(params[:bunker_id])
+    @trip.user = current_user
+    @trip.booking_date = Date.today
 
     if @trip.save
       redirect_to bunker_path(@bunker)
@@ -39,6 +39,6 @@ private
     @trip = Trip.find(params[:id])
   end
   def trip_params
-    params.require(:trip).permit(:user_id, :bunker_id, :booking_date, :start_date, :end_date)
+    params.require(:trip).permit(:start_date, :end_date)
   end
 end
