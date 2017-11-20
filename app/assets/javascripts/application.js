d5235d5ab75fd6026e58e062a85ffe604f49874f
Grace Chen <aekmot@gmail.com>
2017-11-20 19:23:54 -0200

    homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,47 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+// $("#slideshow > div:gt(0)").hide();
+
+// setInterval(function() {
+//   $('#slideshow > div:first')
+//     .fadeOut(1000)
+//     .next()
+//     .fadeIn(1000)
+//     .end()
+//     .appendTo('#slideshow');
+// }, 3000);
+
+var timer = setInterval(nextImage, 4000);
+var curImage = 0;
+var numImages = 3;
+
+function nextImage() {
+    var e;
+    // remove showMe class from current image
+    e = document.getElementById("slideimg" + curImage);
+    removeClass(e, "showMe");
+
+    // compute next image
+    curImage++;
+    if (curImage > numImages - 1) {
+        curImage = 0;
+    }
+
+    // add showMe class to next image
+    e = document.getElementById("slideimg" + curImage);
+    addClass(e, "showMe");
+}
+
+function addClass(elem, name) {
+    var c = elem.className;
+    if (c) c += " ";  // if not blank, add a space separator
+    c += name;
+    elem.className = c;
+}
+
+function removeClass(elem, name) {
+    var c = elem.className;
+    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
+}
