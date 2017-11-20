e58bd6854843086499faf1c80d0cdc061fd0cb84
Alexander Clapp <alexanderclapp@gmail.com>
2017-11-20 15:04:41 -0200

    added core models including user and location

--- db/migrate/20171120163146_create_bunkers.rb
+++ db/migrate/20171120163146_create_bunkers.rb
@@ -1,10 +1,10 @@
 class CreateBunkers < ActiveRecord::Migration[5.1]
   def change
     create_table :bunkers do |t|
-      t.referencessize :user
+      t.references :user, foreign_key: true
+      t.string :size
       t.integer :price
       t.references :location, foreign_key: true
-      t.references :review, foreign_key: true
 
       t.timestamps
     end
