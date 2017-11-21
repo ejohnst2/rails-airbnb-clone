9ac2f756641b7539b83e06db27756b1bb7645cbb
Geraldo Costa <gecosta7@gmail.com>
2017-11-21 10:20:43 -0200

    trip-views-skeleton-show-index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   resources :bunkers do
     resources :trips, only: [:create]
   end
-  resources :trips, only: [:update, :edit, :destroy]
+  resources :trips, only: [:update, :edit, :destroy, :show]
+  get 'my_trips', to: 'trips#index'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
+
