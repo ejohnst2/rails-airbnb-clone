7cf74d2862506546a878f69716edd7b919fefe13
elijah johnston <eli@millinno.com>
2017-11-21 15:21:29 -0200

    fluid layout for show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,4 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require_tree .
-
-// $("#slideshow > div:gt(0)").hide();
-
-// setInterval(function() {
-//   $('#slideshow > div:first')
-//     .fadeOut(1000)
-//     .next()
-//     .fadeIn(1000)
-//     .end()
-//     .appendTo('#slideshow');
-// }, 3000);
-
-
