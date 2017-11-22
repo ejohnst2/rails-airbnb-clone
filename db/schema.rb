ffdd4accd43baa7f48f5fca83c45e27d413d4f9c
elijah johnston <eli@millinno.com>
2017-11-22 14:35:34 -0200

    got pictures sliding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121181258) do
+ActiveRecord::Schema.define(version: 20171122155248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171121181258) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "name"
+    t.string "short_description"
     t.index ["location_id"], name: "index_bunkers_on_location_id"
     t.index ["user_id"], name: "index_bunkers_on_user_id"
   end
